8c2a97fe7a0c20304304c1f6f2e72adab9c58382
Ryan Castellucci <code@ryanc.org>
2023-12-31 20:19:04 +0000

    download rfc6234 for verification

--- scripts/sha2_const.py
+++ scripts/sha2_const.py
@@ -132,34 +132,58 @@ dgst = check.hexdigest()
 if good is not None and dgst != good:
     raise ValueError('Incorrect constants generated!')
 
+Path(SCRIPT_DIR.parent, 'data').mkdir(exist_ok=True)
+rfc_file = Path(SCRIPT_DIR.parent, 'data', 'rfc6234.txt.xz')
+rfc_hash = '8f39f02a57bfd1da15634706724a585766e6223f77ecdaf243cad16cd3a1aa1b'
+if not rfc_file.exists():
+    from urllib.request import urlopen, Request
+    try:
+        rfc = urlopen(Request(
+            'https://www.rfc-editor.org/rfc/rfc6234.txt',
+            headers={'User-Agent':'Mozilla/5.0'}
+        )).read()
+        chk = sha256(rfc).hexdigest()
+        if chk != rfc_hash:
+            raise ValueError(f'RFC6234 data invalid: SHA256:{chk}')
+
+        with lzma.open(rfc_file, 'wb') as f:
+            f.write(rfc)
+    except Exception as e:
+        eprint(e)
+
 rfc_values = {}
-with lzma.open(Path(SCRIPT_DIR.parent, 'data', 'rfc6234.txt.xz'), 'rt') as f:
-    rfc_section, rfc_key = None, None
-    hex2int = lambda x: int(x, 16)
-    # extract constant values from rfc
-    for line in map(str.strip, f):
-        if m := re.match(r'((?:\d+[.])+)\s{2,99}([A-Z].*)', line):
-            rfc_section = m.group(1)
-        elif rfc_section == '5.1.':
-            if m := re.match((r'([0-9a-f]{8})\s+' * 4)[:-3], line):
-                data = rfc_values.setdefault('K32', [])
-                for v in map(hex2int, m.groups()): data.append(v)
-        elif rfc_section == '5.2.':
-            if m := re.match((r'([0-9a-f]{16})\s+' * 4)[:-3], line):
-                data = rfc_values.setdefault('K64', [])
-                for v in map(hex2int, m.groups()): data.append(v)
-        elif rfc_section == '6.1.':
-            if rfc_key is None: rfc_key = 'IV224'
-            if m := re.match(r'H\(0\)\d\s+=\s+([0-9a-f]{8})', line):
-                data = rfc_values.setdefault(rfc_key, [])
-                data.append(hex2int(m.group(1)))
-                if len(data) == 8: rfc_key = 'IV256'
-        elif rfc_section == '6.3.':
-            if rfc_key == 'IV256': rfc_key = 'IV384'
-            if m := re.match(r'H\(0\)\d\s+=\s+([0-9a-f]{16})', line):
-                data = rfc_values.setdefault(rfc_key, [])
-                data.append(hex2int(m.group(1)))
-                if len(data) == 8: rfc_key = 'IV512'
+try:
+    with lzma.open(rfc_file, 'rt') as f:
+        rfc_section, rfc_key = None, None
+        hex2int = lambda x: int(x, 16)
+        # extract constant values from rfc
+        for line in map(str.strip, f):
+            if m := re.match(r'((?:\d+[.])+)\s{2,99}([A-Z].*)', line):
+                rfc_section = m.group(1)
+            elif rfc_section == '5.1.':
+                if m := re.match((r'([0-9a-f]{8})\s+' * 4)[:-3], line):
+                    data = rfc_values.setdefault('K32', [])
+                    for v in map(hex2int, m.groups()): data.append(v)
+            elif rfc_section == '5.2.':
+                if m := re.match((r'([0-9a-f]{16})\s+' * 4)[:-3], line):
+                    data = rfc_values.setdefault('K64', [])
+                    for v in map(hex2int, m.groups()): data.append(v)
+            elif rfc_section == '6.1.':
+                if rfc_key is None: rfc_key = 'IV224'
+                if m := re.match(r'H\(0\)\d\s+=\s+([0-9a-f]{8})', line):
+                    data = rfc_values.setdefault(rfc_key, [])
+                    data.append(hex2int(m.group(1)))
+                    if len(data) == 8: rfc_key = 'IV256'
+            elif rfc_section == '6.3.':
+                if rfc_key == 'IV256': rfc_key = 'IV384'
+                if m := re.match(r'H\(0\)\d\s+=\s+([0-9a-f]{16})', line):
+                    data = rfc_values.setdefault(rfc_key, [])
+                    data.append(hex2int(m.group(1)))
+                    if len(data) == 8: rfc_key = 'IV512'
+
+except Exception as e:
+    eprint(e)
+    rfc_values = None
 
 class IncorrectConstant(ValueError):
     def __init__(self, name, index, actual, expected):
@@ -167,43 +191,48 @@ class IncorrectConstant(ValueError):
         message = f'{name}[{index}] is {{0:{fmt}}}, should be {{1:{fmt}}}'
         super().__init__(message.format(actual, expected))
 
+if rfc_values is not None:
+    def verify(name, key, i, x):
+        if x != rfc_values[key][i]:
+            raise IncorrectConstant(name, i, x, rfc_values[key][i])
+
+    for i, x in map(lambda i: (i, IV[i+8] & ((1 << 32) - 1)), range(8)):
+        verify('SHA224_IV', 'IV224', i, x)
+
+    for i, x in map(lambda i: (i, IV[i] >> 32), range(8)):
+        verify('SHA256_IV', 'IV256', i, x)
+
+    for i, x in map(lambda i: (i, IV[i+8]), range(8)):
+        verify('SHA384_IV', 'IV384', i, x)
+
+    for i, x in map(lambda i: (i, IV[i]), range(8)):
+        verify('SHA512_IV', 'IV512', i, x)
+
+    for i, x in map(lambda i: (i, K[i] >> 32), range(64)):
+        verify('SHA256_K', 'K32', i, x)
+
+    for i, x in map(lambda i: (i, K[i]), range(80)):
+        verify('SHA512_K', 'K64', i, x)
+
 print('#pragma once\n')
 print('/*', dgst, '*/\n')
 
-for i in range(8):
-    x = IV[i+8] & ((1 << 32) - 1)
-    if x != rfc_values['IV224'][i]:
-        raise IncorrectConstant('SHA224_IV', i, x, rfc_values['IV224'][i])
+for i, x in map(lambda i: (i, IV[i+8] & ((1 << 32) - 1)), range(8)):
     print(f'#define SHA224_IV{i} 0x{x:08X}UL')
 
-for i in range(8):
-    x = IV[i] >> 32
-    if x != rfc_values['IV256'][i]:
-        raise IncorrectConstant('SHA256_IV', i, x, rfc_values['IV256'][i])
+for i, x in map(lambda i: (i, IV[i] >> 32), range(8)):
     print(f'#define SHA256_IV{i} 0x{x:08X}UL')
 
-for i in range(8):
-    x = IV[i+8]
-    if x != rfc_values['IV384'][i]:
-        raise IncorrectConstant('SHA384_IV', i, x, rfc_values['IV384'][i])
+for i, x in map(lambda i: (i, IV[i+8]), range(8)):
     print(f'#define SHA384_IV{i} 0x{x:016X}ULL')
 
-for i in range(8):
-    x = IV[i]
-    if x != rfc_values['IV512'][i]:
-        raise IncorrectConstant('SHA512_IV', i, x, rfc_values['IV512'][i])
+for i, x in map(lambda i: (i, IV[i]), range(8)):
     print(f'#define SHA512_IV{i} 0x{x:016X}ULL')
 
-for i in range(64):
-    x = K[i] >> 32
-    if x != rfc_values['K32'][i]:
-        raise IncorrectConstant('SHA256_K', i, x, rfc_values['K32'][i])
+for i, x in map(lambda i: (i, K[i] >> 32), range(64)):
     print(f'#define SHA256_K{i:02} 0x{x:08X}UL')
 
-for i in range(80):
-    x = K[i]
-    if x != rfc_values['K64'][i]:
-        raise IncorrectConstant('SHA512_K', i, x, rfc_values['K64'][i])
+for i, x in map(lambda i: (i, K[i]), range(80)):
     print(f'#define SHA512_K{i:02} 0x{x:016X}ULL')
 
 for i in range(64):
